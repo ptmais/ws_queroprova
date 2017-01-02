0653bb720573d437997435cf39dcca9c1fdaa987
ptmais <alexandre.aprendiz@gmail.com>
2017-01-02 14:27:49 -0200

    define as mensagens de retorno

---
+++ dao/Mensagens.php
@@ -0,0 +1,29 @@
+<?php
+
+define('USUARIO_INSERIDO_SUCESSO', 'Conta cadastrada com sucesso.');
+define('USUARIO_DELETADO_SUCESSO', 'Conta deletada com sucesso.');
+define('USUARIO_DELETADO_FRACASSO', 'Nenhuma conta encontrada.');
+define('USUARIO_ATUALIZADO_SUCESSO', 'Conta atualizada com sucesso.');
+define('USUARIO_ATUALIZADO_FRACASSO', 'Nenhuma alteração foi realizada.');
+define('USUARIO_NAO_ENCONTRADO', 'Nenhum usuário encontrado.');
+
+define('ARQUIVO_INSERIDO_SUCESSO', 'Arquivo enviado com sucesso.');
+define('ARQUIVO_DELETADO_SUCESSO', 'Arquivo deletado com sucesso.');
+define('ARQUIVO_DELETADO_FRACASSO', 'Nenhum arquivo encontrado.');
+define('ARQUIVO_ATUALIZADO_SUCESSO', 'Arquivo atualizado com sucesso.');
+define('ARQUIVO_ATUALIZADO_FRACASSO', 'Nenhuma alteração foi realizada.');
+define('ARQUIVO_NAO_ENCONTRADO', 'Nenhum arquivo encontrado.');
+
+define('EVENTO_INSERIDO_SUCESSO', 'Evento cadastrado com sucesso.');
+define('EVENTO_DELETADO_SUCESSO', 'Evento deletado com sucesso.');
+define('EVENTO_DELETADO_FRACASSO', 'Nenhum evento encontrado.');
+define('EVENTO_ATUALIZADO_SUCESSO', 'Evento atualizado com sucesso.');
+define('EVENTO_ATUALIZADO_FRACASSO', 'Nenhuma alteração foi realizada.');
+define('EVENTO_NAO_ENCONTRADO', 'Nenhum evento encontrado.');
+
+define('DISCIPLINA_INSERIDO_SUCESSO', 'Disciplina cadastrada com sucesso.');
+define('DISCIPLINA_DELETADO_SUCESSO', 'Disciplina deletada com sucesso.');
+define('DISCIPLINA_DELETADO_FRACASSO', 'Nenhuma disciplina encontrada.');
+define('DISCIPLINA_ATUALIZADO_SUCESSO', 'Disciplina atualizada com sucesso.');
+define('DISCIPLINA_ATUALIZADO_FRACASSO', 'Nenhuma alteração foi realizada.');
+define('DISCIPLINA_NAO_ENCONTRADO', 'Nenhuma disciplina encontrada.');
